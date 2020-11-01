27ba86f707bbf1be16f686870cb5ebee32973cc1
Gurkiran Singh <gurkiransinghk@gmail.com>
2020-11-01 17:44:21 -0500

    removed the right homepage before user is logged in

--- src/App.js
+++ src/App.js
@@ -38,9 +38,13 @@ function App(props) {
             <svg onClick={handleLeftToggle} width="1em" height="1em" viewBox="0 0 16 16" className="bi bi-filter-left" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
                 <path fillRule="evenodd" d="M2 10.5a.5.5 0 0 1 .5-.5h3a.5.5 0 0 1 0 1h-3a.5.5 0 0 1-.5-.5zm0-3a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7a.5.5 0 0 1-.5-.5zm0-3a.5.5 0 0 1 .5-.5h11a.5.5 0 0 1 0 1h-11a.5.5 0 0 1-.5-.5z"/>
             </svg>
-            <svg onClick={handleRightToggle} width="1em" height="1em" viewBox="0 0 16 16" className="bi bi-filter-right" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
-              <path fillRule="evenodd" d="M14 10.5a.5.5 0 0 0-.5-.5h-3a.5.5 0 0 0 0 1h3a.5.5 0 0 0 .5-.5zm0-3a.5.5 0 0 0-.5-.5h-7a.5.5 0 0 0 0 1h7a.5.5 0 0 0 .5-.5zm0-3a.5.5 0 0 0-.5-.5h-11a.5.5 0 0 0 0 1h11a.5.5 0 0 0 .5-.5z"/>
-            </svg>
+            {
+              props.currentUser && (
+                <svg onClick={handleRightToggle} width="1em" height="1em" viewBox="0 0 16 16" className="bi bi-filter-right" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                  <path fillRule="evenodd" d="M14 10.5a.5.5 0 0 0-.5-.5h-3a.5.5 0 0 0 0 1h3a.5.5 0 0 0 .5-.5zm0-3a.5.5 0 0 0-.5-.5h-7a.5.5 0 0 0 0 1h7a.5.5 0 0 0 .5-.5zm0-3a.5.5 0 0 0-.5-.5h-11a.5.5 0 0 0 0 1h11a.5.5 0 0 0 .5-.5z"/>
+                </svg>
+              )
+            }
           </div>
           <div className="row">
             <Header leftHomepageRef={leftHomepageRef} rightHomepageRef={rightHomepageRef} />
