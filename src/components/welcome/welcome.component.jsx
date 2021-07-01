5d170de90a83e67a326f00c53195016f27f3b3fc
gurkiran_singh <gurkiransinghk@gmail.com>
2021-06-30 22:03:34 -0400

    Removed the welcome page text
    Signed-off-by: gurkiran_singh <gurkiransinghk@gmail.com>

--- src/components/welcome/welcome.component.jsx
+++ src/components/welcome/welcome.component.jsx
@@ -8,7 +8,6 @@ export default function Welcome() {
             <div className="text-center">
                 <h2>Welcome to the Dev-World.</h2>
                 <h4>In this platform , users can create posts and share their stories to different developers.</h4>
-                <h4>This Project is inspired from <a href="https://www.linkedin.com/in/abhiram-reddy-23285b196/" target="_blank" rel="noopener noreferrer"> Abhiram Reddy </a> who was the finalist of job Challenge v1.</h4>
             </div>
         </div>
     )
